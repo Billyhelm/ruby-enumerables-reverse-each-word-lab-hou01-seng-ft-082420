9a0acc5e261865f07ff9bd03c4e05fe02601191c
Billy Helm <billyhelm88@gmail.com>
2020-07-28 18:50:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(phrase)
   new_array.collect do |word|
     word.reverse
   end 
-  p blah.join(" ")
+  p new_array.join(" ")
 end 
 
 reverse_each_word("Hello there, and how are you?")
