14820b4e7b52c8160dcee35c20f9f406d346737f
Billy Helm <billyhelm88@gmail.com>
2020-07-28 18:49:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(phrase)
   new_array = phrase.split(/ /)
-  blah = []
-  new_array.each do |word|
-    blah.push(word.reverse)
+  new_array.collect do |word|
+    word.reverse
   end 
   blah.join(" ")
 end 
