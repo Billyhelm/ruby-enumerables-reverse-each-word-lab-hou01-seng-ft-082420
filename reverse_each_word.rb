c9106ccb3b3d23b0690580c8b23e682e6d88c439
Billy Helm <billyhelm88@gmail.com>
2020-07-28 18:29:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
 def reverse_each_word(phrase)
   new_array = []
-  new_word = []
-  new_array.push(phrase.split(//)) 
+  new_array.push(phrase.split(/ /)) 
   p new_array
-  p new_word
 end 
 
 reverse_each_word("Hello there, and how are you?")
